52b20d09a685d264c28dc2f9f3d64d83e18aa6cf
Juan Carlos Hernandez Perez <libra_jchp@hotmail.com>
2017-04-03 14:43:00 -0500

    Se agregó la vista estilo acordeon de las actas para los años 2014, 2015, 2016, 2017, de igual manera todos los meses respectivamente

--- protected/views/sociosPersonaFisica/_partial.php
+++ protected/views/sociosPersonaFisica/_partial.php
@@ -3,17 +3,22 @@
 
 
 
-
-<?php echo $anio; ?>
-<?php $this->widget('zii.widgets.jui.CJuiAccordion',
-array(
-	'panels'=>array(
-		'Enero'  =>$this->renderPartial('_data',
-							array('anio'=>$anio,'mes'=>1),true),
-		'Febrero'=>$this->renderPartial('_data',
-							array('anio'=>$anio,'mes'=>2),true),
-		'Marzo'  =>$this->renderPartial('_data',
-							array('anio'=>$anio,'mes'=>3),true),
-	),
+<?php echo $anio;?>
+<?php $this->widget ('zii.widgets.jui.CJuiAccordion',
+ array(
+ 'panels'=>array(
+	'Enero' =>  $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>1), true),
+	'Febrero' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>2), true),
+	'Marzo' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>3), true),
+	'Abril' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>4), true),
+	'Mayo' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>5), true),
+	'Junio' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>6), true),
+	'Julio' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>7), true),
+	'Agosto' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>8), true),
+	'Septiembre' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>9), true),
+	'Octubre' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>10), true),
+	'Noviembre' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>11), true),
+	'Diciembre' => $this->renderPartial('_data', array('anio'=>$anio, 'mes'=>12), true),
+		),
 ));
 ?>
